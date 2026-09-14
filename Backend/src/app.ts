1e17f62de215adb3ce656ea63d59fefc51ac1742
AnantKumarSingh26 <anantsingh.code@gmail.com>
2026-09-14 14:27:05 +0530

    Add Zod validation and integrate AI models in graph service

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -1,9 +1,12 @@
 import express from "express";
-
+import useGraph from './services/graph.ai.service.js'
 const app = express();
 
-app.get('/health',(req,res)=>{
-    res.status(200).json({status:'ok'})
+app.get('/health', (req, res) => {
+    res.status(200).json({ status: 'ok' })
 })
 
+app.post('/use-graph',async (req, res) => {
+    await useGraph('What is the capital of France?').then()
+})
 export default app
--- Backend/src/services/model.service.ts
+++ Backend/src/services/model.service.ts
@@ -1,21 +1,22 @@
+import config from "../config/config.js";
 import { ChatGoogle } from "@langchain/google";
 import { ChatMistralAI } from "@langchain/mistralai";
 import { ChatCohere } from "@langchain/cohere"
 
-import config from "../config/config.js";
 
 export const geminiModel = new ChatGoogle({
     apiKey: config.GOOGLE_API_KEY,
+
     model: "gemini-2.5-flash-lite",
 });
 
-const mistralModel = new ChatMistralAI({
-    model: "mistral-medium-latest",
+export const mistralModel = new ChatMistralAI({
+    model: "open-mistral-7b",
     temperature: 0,
     apiKey: config.MISTRAL_API_KEY
 });
 
-export const cohereModel = ({
+export const cohereModel =new ChatCohere({
     model: "command-r-plus",
     temperature: 0,
     maxRetries: 2,
