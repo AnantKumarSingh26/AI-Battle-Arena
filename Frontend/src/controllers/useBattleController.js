e68ccd3050848d18e1ed9bc5a5bf9e11e6715d19
AnantKumarSingh26 <anantsingh.code@gmail.com>
2026-09-19 16:35:39 +0530

    Add useBattleController hook and integrate with Arena component; update package.json and package-lock.json for new dependency

---
+++ Frontend/src/controllers/useBattleController.js
@@ -0,0 +1,43 @@
+import { useState } from "react";
+
+export const useBattleController = () => {
+    const [battleData, setBattleData] = useState(null)
+    const [isLoading, setIsLoading] = useState(false)
+    const [errar, setErrar] = useState(null)
+
+    const handleStartBattle = async (question) => {
+
+        try {
+            setTimeout(() => {
+                const dummyResponse = {
+                    problem: question,
+                    solution_1: "# Program to Calculate Factorial of a Number\n\nHere's a simple program to calculate the factorial of a number in several programming languages...",
+                    solution_2: "Here's a Python script with a self-contained function that solves the problem and passes corresponding tests:\n```python\ndef factorial(n):\n    if n == 0:\n        return 1\n    else:\n        return n * factorial(n - 1)\n```",
+                    judge: {
+                        solution_1_score: 10,
+                        solution_2_score: 5,
+                        solution_1_reasoning: "Solution 1 provides a comprehensive and highly useful response by demonstrating the implementation...",
+                        solution_2_reasoning: "Solution 2 provides only a single function in Python without any input/output handling or testing code..."
+                    }
+                }
+                setBattleData(dummyResponse)
+                setIsLoading(false)
+            }, 1500);
+
+        } catch (err) {
+            setErrar('Error in getting data from Backend',err)
+            setIsLoading(false)
+        }
+    }
+    const handleNewChat = () => {
+        setBattleData(null);
+        setErrar(null)
+    }
+    return {
+        battleData,
+        isLoading,
+        errar,
+        handleStartBattle,
+        handleNewChat
+    }
+}
--- Frontend/src/views/arena.jsx
+++ Frontend/src/views/arena.jsx
@@ -4,31 +4,14 @@ import PromptInput from "./promptInput";
 import Sidebar from "./sidebar";
 import JudgePanel from "./JudgePanel";
 import ResponseCard from "./responseCard";
+import { useBattleController } from "../controllers/useBattleController";
 
 const Arena = () => {
-    //state handling for battle results
-    const [battleData, setBattleData] = useState(null)
-    // New Chat Handling
-    const handleNewChat = () => {
-        setBattleData(null)
-    }
-    // Response of API from Backend
-    const handleStartBattle = (question) => {
-        // Abhi ke liye API call ki jagah hum dummy data set kar rahe hain.
-        // Baad me yahan Axios fetch aayega.
-        const dummyResponse = {
-            problem: question,
-            solution_1: "# Program to Calculate Factorial of a Number\n\nHere's a simple program to calculate the factorial of a number in several programming languages...",
-            solution_2: "Here's a Python script with a self-contained function that solves the problem and passes corresponding tests:\n```python\ndef factorial(n):\n    if n == 0:\n        return 1\n    else:\n        return n * factorial(n - 1)\n```",
-            judge: {
-                solution_1_score: 10,
-                solution_2_score: 5,
-                solution_1_reasoning: "Solution 1 provides a comprehensive and highly useful response by demonstrating the implementation...",
-                solution_2_reasoning: "Solution 2 provides only a single function in Python without any input/output handling or testing code..."
-            }
-        };
-        setBattleData(dummyResponse);
-    };
+    const { battleData,
+        isLoading,
+        errar,
+        handleStartBattle,
+        handleNewChat } = useBattleController
 
     let winner = "Tie";
     if (battleData) {
@@ -42,7 +25,8 @@ const Arena = () => {
             <Sidebar onNewChat={handleNewChat} />
 
             {/* Right Side me main component Area */}
-            <main className="flex-1 flex flex-col items-center p-8 overflow-y-auto">
+            <main className="flex-1 flex flex-col items-center p-8 overflow-y-auto scrollbar-thin scrollbar-thumb-gray-50
+           ">
                 {/* Title Area */}
                 <div className="text-center mb-12 mt-8">
                     <h1 className="text-4xl font-extrabold mb-3 text-white tracking-widest drop-shadow-md">AI Battle Arena</h1>
